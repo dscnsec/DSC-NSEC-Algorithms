2f2e5418d23e6cf43773d0200518e1236d1e8cd8
cSubhradipta <csubhradipta@gmail.com>
2020-12-17 19:58:11 +0530

    Update [CPP]priority_tasks_csubhradipta.cpp

--- 4. Stack/priority_tasks/[CPP]priority_tasks_csubhradipta.cpp
+++ 4. Stack/priority_tasks/[CPP]priority_tasks_csubhradipta.cpp
@@ -3,12 +3,9 @@
  * @brief: Find task with the next greater priority number
  * @details: 
  * For checking higher priority task than ith task,
- * the array is travesed from (i+1)th task to nth task
+ * the array is travesed from (i+1)th task to (n+i)th task 
  * If got a higher priority task than ith task, then it is pushed into the stack
  * Else pushed -1 
- * when again searching from beginning,
- * If got a higher priority task than the last task, it is pushed into the stack
- * Else pushed -1
  * Reversed the stack
  * Space Complexity : O(n)
  * Time Complexity : O(n)
@@ -29,32 +26,18 @@ void solve(){
     stack<int> tasks, output;
     int high;
     
-    for (int i = 0; i < n-1; i++) 
-    { 
-        high = -1;                          // will be returned if not get a higher priority task
-        for (int j = i+1 ; j < n; j++) 
-        {   if ((priority[i] < priority[j]) && j != 0) 
-            { 
-                high = priority[j];         // got task of higher priority
+    for (int i = 0; i < n; i++) 
+    {   high = -1;
+        for (int j = i+1 ; j < n+i;  j++)       //checking n no. of tasks from next position  
+        {   if ((priority[i] < priority[j%n])) 
+            {   
+                high = priority[j%n];         // got task of higher priority
                 break;
             }
         } 
-        tasks.push(high); 
-    }
-    
-    // Again, searching from beginning
-    
-    high = -1;
-    
-    for (int i=0; i<n; i++){
-        if(priority[n-1] < priority[i]){
-            high = priority[i];             // got task of higher priority
-            break;
-        }
-    }
-    
-    tasks.push(high);   
-    
+        tasks.push(high);
+    } 
+
     while(!tasks.empty()){
         output.push(tasks.top());           // reversed the stack
         tasks.pop();
