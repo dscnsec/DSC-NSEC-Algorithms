20d9d95d037c032db82971fe6799b9c172389183
cSubhradipta <csubhradipta@gmail.com>
2020-12-19 11:43:32 +0530

    Update [CPP]priority_tasks_csubhradipta.cpp

--- 4. Stack/priority_tasks/[CPP]priority_tasks_csubhradipta.cpp
+++ 4. Stack/priority_tasks/[CPP]priority_tasks_csubhradipta.cpp
@@ -2,11 +2,13 @@
  * @file: [CPP]priority_tasks_csubhradipta.cpp 
  * @brief: Find task with the next greater priority number
  * @details: 
- * For checking higher priority task than ith task,
- * the array is travesed from (i+1)th task to (n+i)th task 
- * If got a higher priority task than ith task, then it is pushed into the stack
- * Else pushed -1 
- * Reversed the stack
+ * As the array is circular so we have to traverse it twice, therefore the array is concatinated to itself
+ * The resultant array initialized with -1, so that if no task foun of higher priority, it will remain -1
+ * Else it will be updated with the higher priority task
+ * Putting index of element in stack, not the element as similar element may exist
+ * While stack is not empty and current task's priority is lesser than previous task's priority,
+ * its index being pushed to the stack
+ * If higher priority task found, the resultant array gets updated
  * Space Complexity : O(n)
  * Time Complexity : O(n)
  * @author [Subhradipta Choudhury](https://github.com/csubhradipta)
@@ -17,37 +19,31 @@ using namespace std;
 
 void solve(){
     int n;
-    cin>>n;
-    vector<int> priority(n);  
+    cin >> n;
+    
+    int priority[2*n], result[n]; 
     for(int i = 0; i < n; i++){
-        cin>>priority[i];
+        cin >> priority[i];
+        priority[i+n] = priority[i];          // concatinating array to itself
+        result[i] = -1;
     }
     
-    stack<int> tasks, output;
-    int high;
+    stack<int> task;
     
-    for (int i = 0; i < n; i++) 
-    {   high = -1;
-        for (int j = i+1 ; j < n+i;  j++)       //checking n no. of tasks from next position  
-        {   if ((priority[i] < priority[j%n])) 
+    for(int i = 0; i < 2*n; i++)
+    {   
+            while(!task.empty() && priority[i] > priority[task.top()])      // getting a higher priority task
             {   
-                high = priority[j%n];         // got task of higher priority
-                break;
+                result[task.top()%n] = priority[i];      // store the higher priority task
+                task.pop();
             }
-        } 
-        tasks.push(high);
-    } 
-
-    while(!tasks.empty()){
-        output.push(tasks.top());           // reversed the stack
-        tasks.pop();
+        task.push(i);                // push the current index
     }
     
-    while(!output.empty()){
-        cout<<output.top()<<" ";
-        output.pop();
-    }
-    cout<<endl;    
+    for(int i = 0; i < n; i++)
+        cout<<result[i]<<" ";
+        
+    cout<<endl;  
 }
 
 int main() 
