db70cb838c54aab00a9c1c143c6b995b3b00bf63
cSubhradipta <csubhradipta@gmail.com>
2020-12-20 07:26:50 +0530

    Update [CPP]mid_csubhradipta.cpp

--- 5. Linked List/mid/[CPP]mid_csubhradipta.cpp
+++ 5. Linked List/mid/[CPP]mid_csubhradipta.cpp
@@ -3,8 +3,10 @@
  * @brief: Find middle node of a linked list and show its value
  * @details:
  * INSERT elements (first taking input for number of nodes from user)
- * COUNT number of nodes and set MID = COUNT/2 + 1
- * Iterate till MID node and print its value
+ * Take two node pointers and initialize them from start
+ * fast will traverse two step while slow will one 
+ * and so, by the time fast reaches to null, slow will just reach the middle
+ * print the node element where slow points
  * Space Complexity : O(n)
  * Time Complexity : O(n)
  * @author [Subhradipta Choudhury](https://github.com/csubhradipta)
@@ -24,44 +26,33 @@ Node *start;    // Declared start pointer globally
 int main() 
 { 
     int n, data; // for number of nodes
-    cin>>n;		// taking number of nodes as input from user
+    cin>>n;     // taking number of nodes as input from user
     
     Node *p;
     while(n--){
-    	cin>>data;      // element to be inserted
-    	p = start;
+        cin>>data;      // element to be inserted
+        p = start;
         Node *temp = new Node;
-    	temp->val = data;
-    	temp->next = NULL;
-    	if(!start)
-    	    start = temp;
-    	else{
-    	    while (p->next != NULL)     // adding node to linked list
-    	        p = p->next;
-    	    p->next = temp;
-    	    p = temp;
-    	}
-    }
-    
-    int count = 0;
-    
-    p = start;
-    while(p != NULL){       // counting number of nodes
-    	p = p->next;
-        count++;
-    }
-    
-    int mid = count/2 + 1, i = 1;   // finding mid postion
-    
-    p = start;
-    while(p != NULL){      // iterating till mid position
-        if(i == mid){
-    	    cout<<p->val;
-    	    break;
+        temp->val = data;
+        temp->next = NULL;
+        if(!start)
+            start = temp;
+        else{
+            while (p->next != NULL)     // adding node to linked list
+                p = p->next;
+            p->next = temp;
+            p = temp;
         }
-    	p = p->next;
-    	i++;
     }
+    
+    Node *fast = start, *slow = start;
+    
+    while(fast != NULL && fast->next != NULL){ 
+        fast = fast->next->next;                // moving two step forward
+        slow = slow->next;                      // moving one step forward
+    }
+    
+    cout<<slow->val;
     
     return 0;
 } 
